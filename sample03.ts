2e0c9aaeb51856c4b676be38d0ee47184e6114f5
kaiwarex <pain.of.rebirth@gmail.com>
2025-08-13 17:50:55 +0900

    列挙体の中身について調べたコード

---
+++ sample03.ts
@@ -0,0 +1,22 @@
+//列挙型をつかったケース
+enum finger{goo,choki,paa}
+
+const you_0 = finger.goo;
+const you_1 = finger.choki;
+const you_2 = finger.paa;
+
+// switch(you_0){
+//     case finger.goo:
+//         console.log('あいこです');
+//         break;
+//     case finger.choki:
+//         console.log('あなたの勝ち');
+//         break;
+//     case finger.paa:
+//         console.log('あなたの負け');
+//         break;
+// }
+console.log(you_0 + typeof(you_0));
+console.log(you_1 + typeof(you_1));
+console.log(you_2 + typeof(you_2));
+
